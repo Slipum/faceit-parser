0e1f194128a2f9cf8ef405c6caa993bb56cd0c82
Slipum <123569528+Slipum@users.noreply.github.com>
2024-07-13 21:10:39 +0300

    update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,50 @@
+// For use redis kv
+
+// const axios = require('axios');
+// const { kv } = require('@vercel/kv');
+
+// module.exports = async (req, res) => {
+// 	const targetUrl = req.query.url;
+// 	if (!targetUrl) {
+// 		return res.status(400).send('URL параметр отсутствует');
+// 	}
+
+// 	try {
+// 		const startTime = Date.now();
+// 		console.log(`Запрос кэширования для: ${targetUrl}`);
+
+// 		// Проверяем наличие данных в кэше Vercel KV
+// 		const cacheKey = `cache:${targetUrl}`;
+// 		const cachedData = await kv.get(cacheKey);
+
+// 		if (cachedData) {
+// 			console.log(`Cache hit (time: ${Date.now() - startTime}ms)`);
+// 			return res.json(cachedData);
+// 		}
+
+// 		console.log(`Cache miss (time: ${Date.now() - startTime}ms)`);
+
+// 		// Если данных нет в кэше, выполняем запрос
+// 		const response = await axios.get(targetUrl, { timeout: 15000 });
+
+// 		if (typeof response.data === 'object' && response.data !== null) {
+// 			await kv.set(cacheKey, response.data, {
+// 				ex: 3600, // Время жизни кэша в секундах
+// 			});
+// 			console.log(`Data saved to cache (time: ${Date.now() - startTime}ms)`);
+// 		} else {
+// 			console.error('Полученные данные не являются валидным объектом JSON');
+// 			return res.status(500).send('Ошибка сервера: Невалидные данные');
+// 		}
+
+// 		res.json(response.data);
+// 	} catch (error) {
+// 		console.error('Ошибка при запросе к целевому URL:', error);
+// 		res.status(500).send('Ошибка сервера: ' + error.message);
+// 	}
+// };
+
 const axios = require('axios');
-const { kv } = require('@vercel/kv');
 
 module.exports = async (req, res) => {
 	const targetUrl = req.query.url;
@@ -8,36 +53,10 @@ module.exports = async (req, res) => {
 	}
 
 	try {
-		const startTime = Date.now();
-		console.log(`Запрос кэширования для: ${targetUrl}`);
-
-		// Проверяем наличие данных в кэше Vercel KV
-		const cacheKey = `cache:${targetUrl}`;
-		const cachedData = await kv.get(cacheKey);
-
-		if (cachedData) {
-			console.log(`Cache hit (time: ${Date.now() - startTime}ms)`);
-			return res.json(cachedData);
-		}
-
-		console.log(`Cache miss (time: ${Date.now() - startTime}ms)`);
-
-		// Если данных нет в кэше, выполняем запрос
-		const response = await axios.get(targetUrl, { timeout: 15000 });
-
-		if (typeof response.data === 'object' && response.data !== null) {
-			await kv.set(cacheKey, response.data, {
-				ex: 3600, // Время жизни кэша в секундах
-			});
-			console.log(`Data saved to cache (time: ${Date.now() - startTime}ms)`);
-		} else {
-			console.error('Полученные данные не являются валидным объектом JSON');
-			return res.status(500).send('Ошибка сервера: Невалидные данные');
-		}
-
+		const response = await axios.get(targetUrl);
 		res.json(response.data);
 	} catch (error) {
 		console.error('Ошибка при запросе к целевому URL:', error);
-		res.status(500).send('Ошибка сервера: ' + error.message);
+		res.status(500).send('Ошибка сервера');
 	}
 };
